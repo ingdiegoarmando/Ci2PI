94e2ff0a10426bf70d3fc9f12f2e1354cbaa157b
DV <DV@DESKTOP-6MBC6E6>
2018-01-03 20:07:16 -0500

    Add project files.

---
+++ Ci2.PI.Persistencia/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ci2.PI.Persistencia
+{
+    public class Class1
+    {
+    }
+}
